afd7f5460a1d0aea28dcc53eb9868009cbaeef9e
Moon Ki Jung <m.k.jung@outlook.com>
2020-07-04 02:00:18 +0100

    Add an example.
    Add an example.

---
+++ Examples/Test_Sample00.py
@@ -0,0 +1,23 @@
+import os
+import sys
+if sys.path[0] != os.getcwd():
+    sys.path.insert(0, os.getcwd())
+lib_local_path = os.path.normpath(r'C:\WORKSPACE\DEV\mocaplib')
+if os.path.exists(lib_local_path):
+    if sys.path[1] != lib_local_path:
+        sys.path.insert(1, lib_local_path)
+import mocaplib as mcl
+from mocaplib import btkapp as ba
+import numpy as np
+# import logging
+#%%
+current_f_dir_path = os.path.dirname(__file__)
+c3d_sample_dir_path = os.path.join(current_f_dir_path, r'..\Samples_C3D\Sample00\Vicon Motion Systems')
+src_c3d_path = os.path.join(c3d_sample_dir_path, 'pyCGM2 lower limb CGM24 Walking01.c3d')
+tgt_c3d_path = os.path.splitext(__file__)[0]+'_result.c3d'
+tgt_log_path = os.path.splitext(__file__)[0]+'_result.log'
+
+acq = ba.get_acq(src_c3d_path)
+dict_events = ba.get_dict_events(acq)
+dict_markers = ba.get_dict_markers(acq)
+dict_analogs = ba.get_dict_analogs(acq)
