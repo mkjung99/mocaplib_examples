8507984a080dcdb68dde6fee991317b12c0f50f2
Moon Ki Jung <m.k.jung@outlook.com>
2021-05-12 02:27:04 +0100

    Update Test01_forceplates.py
    Modified the output values according to the change of MoCapLib's btkapp.py.

--- Examples/Test01_forceplates.py
+++ Examples/Test01_forceplates.py
@@ -9,24 +9,24 @@ if os.path.exists(lib_local_path):
 from mocaplib import btkapp as ba
 import numpy as np
 #%%
-current_f_dir_path = os.path.dirname(__file__)
+cwd = os.path.dirname(__file__)
 
-c3d_sample_dir_path = os.path.join(current_f_dir_path, r'..\Samples_C3D\Sample00\Vicon Motion Systems')
+c3d_sample_dir_path = os.path.join(cwd, r'..\Samples_C3D\Sample00\Vicon Motion Systems')
 src_c3d_path = os.path.join(c3d_sample_dir_path, 'pyCGM2 lower limb CGM24 Walking01.c3d')
 
-# c3d_sample_dir_path = os.path.join(current_f_dir_path, r'..\Samples_C3D\Sample00\Codamotion')
-# src_c3d_path = os.path.join(c3d_sample_dir_path, 'codamotion_gaitwands_19970212.c3d')
-
-# c3d_sample_dir_path = os.path.join(current_f_dir_path, r'..\Samples_C3D\Sample01')
+# c3d_sample_dir_path = os.path.join(cwd, r'..\Samples_C3D\Sample01')
 # src_c3d_path = os.path.join(c3d_sample_dir_path, 'Eb015pr.c3d')
 
-# c3d_sample_dir_path = os.path.join(current_f_dir_path, r'..\Samples_C3D\Sample05')
+# c3d_sample_dir_path = os.path.join(cwd, r'..\Samples_C3D\Sample05')
 # src_c3d_path = os.path.join(c3d_sample_dir_path, 'vicon512.c3d')
 
-# c3d_sample_dir_path = os.path.join(current_f_dir_path, r'..\Samples_C3D\Sample10')
+# c3d_sample_dir_path = os.path.join(cwd, r'..\Samples_C3D\Sample00\Codamotion')
+# src_c3d_path = os.path.join(c3d_sample_dir_path, 'codamotion_gaitwands_19970212.c3d')
+
+# c3d_sample_dir_path = os.path.join(cwd, r'..\Samples_C3D\Sample10')
 # src_c3d_path = os.path.join(c3d_sample_dir_path, 'TYPE-4a.c3d')
 
-# c3d_sample_dir_path = os.path.join(current_f_dir_path, r'..\Samples_C3D\Sample28')
+# c3d_sample_dir_path = os.path.join(cwd, r'..\Samples_C3D\Sample28')
 # src_c3d_path = os.path.join(c3d_sample_dir_path, 'type1.c3d')
 
 acq = ba.open_c3d(src_c3d_path)
@@ -34,14 +34,14 @@ acq = ba.open_c3d(src_c3d_path)
 fp_output = ba.get_fp_output(acq, None)
 wc_output = ba.get_fp_wrench(acq, 0.0)
 
-f_global_btk = wc_output[0]['FORCE']
-f_global_manual = fp_output[0]['F_SURF_GLOBAL']
-diff_f_global = f_global_btk-f_global_manual
+f_lab_btk = wc_output[0]['FORCE']
+f_lab_manual = fp_output[0]['F_COP_LAB']
+diff_f_lab = f_lab_btk-f_lab_manual
 
 m_cop_btk = wc_output[0]['MOMENT']*0.001
-m_cop_manual = fp_output[0]['M_COP_GLOBAL']
+m_cop_manual = fp_output[0]['M_COP_LAB']
 diff_m_cop = m_cop_btk-m_cop_manual
 
-cop_global_btk = wc_output[0]['POS']*0.001
-cop_global_manual = fp_output[0]['COP_GLOBAL']
-diff_cop_global = cop_global_btk-cop_global_manual
+cop_lab_btk = wc_output[0]['POS']*0.001
+cop_lab_manual = fp_output[0]['COP_LAB']
+diff_cop_lab = cop_lab_btk-cop_lab_manual
